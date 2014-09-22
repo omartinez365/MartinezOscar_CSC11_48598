aa602978b3bd4b3e06f32e6eded9fa1266f6f05d
omartinez365 <martinez0975@sbcglobal.net>
2014-09-21 23:20:46 -0700

    assignment2

---
+++ Hmwk/Assignment2/assignment2.s
@@ -0,0 +1,32 @@
+/* Perform 11/3 and 11%3 with successive subtraction */ 
+/* With flag off r0 holds a/b and r1 holds a%b switched if flag is on*/
+
+	.global _start
+_start:
+	MOV R2, #11
+	MOV R3, #5
+	MOV R4, #0	@ counter
+	MOV R5, #0	@ sets the flag
+	CMP R5, #0
+	BEQ switch_flag_off
+switch_flag_on:
+	CMP R3, R2
+	SUB R2, R2, R3
+	ADD R4, R4, #1
+	CMP R3, R2
+	SUB R2, R2, R3
+	ADD R4, R4, #1
+	MOV R0, R3
+	MOV R1, R4
+	b end
+switch_flag_off:
+	CMP R3, R2
+	SUB R2, R2, R3
+	ADD R4, R4, #1
+	CMP R3, R2
+	SUB R2, R2, R3
+	ADD R4, R4, #1
+	MOV R0, R4
+	MOV R1, R3
+end:
+	bx lr
